82547aad70ab536572fdaaadd266a16baccf1e79
me@chrisluce.com <me@chrisluce.com@b0a984d7-d44d-0410-83be-41113f6273eb>
2008-05-26 19:53:49 +0000

    Update with working events from django web server to the area chart. *graph is skewed because one of the dates has 870 events*

--- trunk/ProjectVisualization/src/projecthistory/urls.py
+++ trunk/ProjectVisualization/src/projecthistory/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^stats/', 'projecthistory.views.stats'),
+    (r'^crossdomain.xml', 'projecthistory.views.cross_domain'),    
 )
