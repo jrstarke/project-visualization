4f4ff47c07242ffd5737c1b262e38f88b9268761
sillito <sillito@b0a984d7-d44d-0410-83be-41113f6273eb>
2008-05-27 17:38:08 +0000

    views.stats now returns the log_normal_count

--- ProjectVisualization/src/projecthistory/views.py
+++ ProjectVisualization/src/projecthistory/views.py
@@ -1,22 +1,8 @@
 from django.http import HttpResponseRedirect, HttpResponse
-#from django.core import serializers
 from django.utils import simplejson
 from datetime import date, timedelta
 from projecthistory import models
 
-#def tojson(obj):
-#    return serializers.serialize("json", obj)
-
 def stats(request):
-    stats = [(str(s.date), s.commit_events, s.new_ticket_events, s.ticket_change_events)
-        for s in models.DailyStats.objects.all()]
+    stats = [(str(s.date), s.log_normal_count) for s in models.DailyStats.objects.all()]
     return HttpResponse(simplejson.dumps(stats))
-
-def cross_domain(request):
-    return HttpResponse('''
-    <?xml version="1.0"?>
-   <!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">
-   <cross-domain-policy>
-   <allow-access-from domain="*" />
-   </cross-domain-policy>
-    ''')
