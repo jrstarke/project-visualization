5dfadb5158821cf4c0e1e99627beaef75fe4724c
me@chrisluce.com <me@chrisluce.com@b0a984d7-d44d-0410-83be-41113f6273eb>
2008-05-27 19:16:25 +0000

    added back the function for the cross domain request

--- trunk/ProjectVisualization/src/projecthistory/views.py
+++ trunk/ProjectVisualization/src/projecthistory/views.py
@@ -6,3 +6,12 @@ from projecthistory import models
 def stats(request):
     stats = [(str(s.date), s.log_normal_count) for s in models.DailyStats.objects.all()]
     return HttpResponse(simplejson.dumps(stats))
+
+def cross_domain(request):
+    return HttpResponse('''
+    <?xml version="1.0"?>
+   <!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">
+   <cross-domain-policy>
+   <allow-access-from domain="*" />
+   </cross-domain-policy>
+    ''')
