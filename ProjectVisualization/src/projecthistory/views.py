1b514ceb79ee91f0b7464e5e205e1c3f805ea408
sillito <sillito@b0a984d7-d44d-0410-83be-41113f6273eb>
2008-05-29 18:40:51 +0000

    added view to return all events for a given day

--- ProjectVisualization/src/projecthistory/views.py
+++ ProjectVisualization/src/projecthistory/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponseRedirect, HttpResponse
+from django.core import serializers
 from django.utils import simplejson
 from datetime import date, timedelta
+from time import strptime
 from projecthistory import models
+
+def parse_date(date_str):
+    date_str = date_str[:19]
+    return date(*strptime(date_str, '%Y-%m-%d')[0:3])
 
 def stats(request):
     stats = [(str(s.date), s.log_normal_count) for s in models.DailyStats.objects.all()]
     return HttpResponse(simplejson.dumps(stats))
+
+# returns a list of events for the given day
+def events(request, date_str):
+    date = parse_date(date_str)
+    next = date + timedelta(1)
+    all_events = models.events(date, next)
+    return HttpResponse(serializers.serialize('json', iter(all_events)))
 
 def cross_domain(request):
     return HttpResponse('''
