cdb3042da3ec21133fcb042239ef31f4dd661c6a
jrstarke <jrstarke@b0a984d7-d44d-0410-83be-41113f6273eb>
2008-06-24 21:49:53 +0000

    Updated the top authors algorithm

--- trunk/ProjectVisualization/src/projecthistory/models.py
+++ trunk/ProjectVisualization/src/projecthistory/models.py
@@ -214,7 +214,7 @@ def _events(start, stop, model):
 
 def topauthors(start, stop, num_authors):
     author_counts = {}
-    for a in [Author.objects.get(pk=i) for i in [7,1,8,24,40,62,63,30,11,18,28,13,5,54,66,5,54,66,79,162,388,163,429,15,103,14]]:
+    for a in [Author.objects.get(pk=i) for i in [7,1,8,24,40,11,62,63,30,18,28,5,54,66,13,79,162,163,429,82,299,312,15,370,111,103,16,6]]:
         author_counts[a.id] = (a.newticketevent_set.filter(date__range=(start,stop)).count(),a.commitevent_set.filter(date__range=(start,stop)).count(),a.ticketchangeevent_set.filter(date__range=(start,stop)).count())
     sorted_counts = author_counts.items()
     # sorted_counts.sort(lambda a,b: cmp((b[1][0]+b[1][1]+b[1][2]), (a[1][0]+a[1][1]+a[1][2])))
@@ -229,7 +229,7 @@ def topauthors(start, stop, num_authors):
         author["author_shortname"] = authorshortname(author["author_name"])
         top_authors.append(author)
     top_authors.sort(lambda a,b: cmp(a["author_name"].lower(),b["author_name"].lower()))
-    top_authors = filter(lambda x: (x["newticketevents"] + x["commitevents"] + x["ticketchangeevents"]) > 0, top_authors)
+    # top_authors = filter(lambda x: (x["newticketevents"] + x["commitevents"] + x["ticketchangeevents"]) > 0, top_authors)
     return top_authors
 
 def authorshortname(author_name):
