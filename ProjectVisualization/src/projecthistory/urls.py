f17af474b70ecb33338e7bb7a52fb980d0b6c11f
jrstarke <jrstarke@b0a984d7-d44d-0410-83be-41113f6273eb>
2008-07-02 22:00:03 +0000

    added the ability to get daily stats restricted by author_id or module_id

--- ProjectVisualization/src/projecthistory/urls.py
+++ ProjectVisualization/src/projecthistory/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^stats/$', 'projecthistory.views.stats'),
+    (r'^selectedstats/(?P<author_id_str>[\d-]+)/(?P<module_id_str>[\d-]+)/$', 'projecthistory.views.selectedstats'),
     (r'^events/(?P<date_str>[\d-]+)/$', 'projecthistory.views.events'),
     (r'^eventsrange/(?P<date_str_start>[\d-]+)/(?P<date_str_stop>[\d-]+)/$', 'projecthistory.views.eventrange'),
     (r'^topauthors/(?P<start_date>[\d-]+)/(?P<end_date>[\d-]+)/(?P<num_authors>\d+)/$', 'projecthistory.views.topauthors'),
