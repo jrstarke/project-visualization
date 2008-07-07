bffb859c67d3c6d30866e00afd33d3b43b0c938a
jrstarke <jrstarke@b0a984d7-d44d-0410-83be-41113f6273eb>
2008-07-07 20:34:47 +0000

    added status codes for the lists of files

--- ProjectVisualization/src/projecthistory/views.py
+++ ProjectVisualization/src/projecthistory/views.py
@@ -10,9 +10,7 @@ def parse_date(date_str):
     return date(*strptime(date_str, '%Y-%m-%d')[0:3])
 
 def stats(request):
-    #stats = [(str(s.date), s.log_normal_count) for s in models.DailyStats.objects.all()]
-    #return HttpResponse(simplejson.dumps(stats))
-    return HttpResponse(simplejson.dumps(models.daily_stats()))
+    return selectedstats(request,'0','0')
 
 def selectedstats(request, author_id_str, module_id_str):
     author_id = int(author_id_str)
@@ -21,57 +19,11 @@ def selectedstats(request, author_id_str, module_id_str):
 
 # returns a list of events for the given day
 def events(request, date_str):
-    date = parse_date(date_str)
-    next = date + timedelta(1)
-    #all_events = models.events(date, next)
-  
-    all_events = []
-    for event in models.events(date,next):
-        files = []
-        modules = []
-        new_status = ""
-        if event.__class__.__name__ == 'CommitEvent':
-            for p in event.path_set.all():
-                if not files.__contains__(p.dest_file):   
-                    files.append(p.dest_file)
-                if (not modules.__contains__(p.module.id)):
-                    modules.append(p.module.id)     
-        all_events.append({'date':str(event.date)[:str(event.date).rfind(" ")], 'author_short_name':models.authorshortname(event.author.name),
-                   'pk':event.id, 'comment':event.comment, 'author':event.author.id, 'author_name':event.author.name,
-                   'type':event.__class__.__name__.replace('Event', ''), 'files':files, 'modules':modules, 'new_status':new_status}) 
-        
-    #return HttpResponse(serializers.serialize('json', iter(all_events)))
-    return HttpResponse(simplejson.dumps(all_events))
+     return eventrange(request, date_str, date_str)
 
 def eventrange(request, date_str_start, date_str_stop):
-    start = parse_date(date_str_start)
-    stop = parse_date(date_str_stop) + timedelta(1)
-    files = {}
-    modules = {}
-    
-    
-    for p in models.Path.objects.all():
-        if not files.setdefault(p.event_id,[]).__contains__(p.dest_file):
-            files[p.event_id].append(p.dest_file)
-
-        if not modules.setdefault(p.event_id,[]).__contains__(p.module_id):
-            modules[p.event_id].append(p.module_id) 
-    print("completed path")
-    #all_events = models.events(date, next)
-    all_events = []
-    for event in models.events(start,stop):
-      #  if event.__class__.__name__ == 'CommitEvent':
-      #      for p in event.path_set.all():
-      #          if not files.__contains__(p.dest_file):   
-      #              files.append(p.dest_file)
-      #          if (not modules.__contains__(p.module.id)):
-      #              modules.append(p.module.id)
-        all_events.append({'date':str(event.date)[:str(event.date).rfind(" ")], 'author_short_name':models.authorshortname(event.author.name),
-                   'pk':event.id, 'comment':event.comment, 'author':event.author.id, 'author_name':event.author.name,
-                   'modules':modules.get(event.id,[]),'files':files.get(event.id,[])}) 
-                   
-    #return HttpResponse(serializers.serialize('json', iter(all_events)))
-    return HttpResponse(simplejson.dumps(all_events))
+    #return all events for the range, where we don't care about who the author or module are (= 0)
+    return selectedeventrange(request, date_str_start, date_str_stop, '0', '0')
 
 def selectedeventrange(request, date_str_start, date_str_stop, author_id_str, module_id_str):
     start = parse_date(date_str_start)
@@ -79,11 +31,13 @@ def selectedeventrange(request, date_str_start, date_str_stop, author_id_str, mo
     author_id = int(author_id_str)
     module_id = int(module_id_str)
     files = {}
+    status = {}
     modules = {}
     
     for p in models.Path.objects.all():
         if not files.setdefault(p.event_id,[]).__contains__(p.dest_file):
             files[p.event_id].append(p.dest_file)
+            status.setdefault(p.event_id,[]).append(p.action)
 
         if not modules.setdefault(p.event_id,[]).__contains__(p.module_id):
             modules[p.event_id].append(p.module_id) 
@@ -91,7 +45,7 @@ def selectedeventrange(request, date_str_start, date_str_stop, author_id_str, mo
     for event in models.events(start,stop,author_id,module_id):
         all_events.append({'date':str(event.date)[:str(event.date).rfind(" ")], 'author_short_name':models.authorshortname(event.author.name),
                    'pk':event.id, 'comment':event.comment, 'author':event.author.id, 'author_name':event.author.name,
-                   'modules':modules.get(event.id,[]),'files':files.get(event.id,[])}) 
+                   'modules':modules.get(event.id,[]),'files':files.get(event.id,[]), 'status':status.get(event.id,[])}) 
                    
     return HttpResponse(simplejson.dumps(all_events))
 
