9ca8e0d5712c853dfe276de4fa3cb70aea30f291
jrstarke <jrstarke@b0a984d7-d44d-0410-83be-41113f6273eb>
2008-08-13 16:03:44 +0000

    added author distributions

--- RepositoryStats/src/information.py
+++ RepositoryStats/src/information.py
@@ -8,5 +8,14 @@ def svnstats(location):
     
     author_commits = {}
     for commit in commits:
-        author_commits[commit.author] = author_commits.setdefault(commit.author,[]).append(commit)
-    print author_commits    
+        author_commits.setdefault(commit.author.name,[]).append(commit)
+    items = author_commits.items()
+    author_counts = []
+    for item in items:
+        author_counts.append((item[0],len(item[1])))
+    author_counts.sort(lambda a,b: cmp(b[1],a[1]))
+    print "commit distribution:"
+    print author_counts                    
+            
+
+    
